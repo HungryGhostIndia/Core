5ccc5cb83e64fb632adfd10a299d9d651324d63c
yash gupta <yash@akeo.no>
2018-03-18 01:57:51 +0530

    localconfig change

--- configs/config.local.js
+++ configs/config.local.js
@@ -16,6 +16,6 @@ module.exports = {
     issuer: 'https://hrSystemApi.com'
   },
   database: {
-    mongoDb: 'mongodb://hungryghost:D3v3l0p3r@ds115799.mlab.com:15799/hungry-ghost',
+    mongoDb: 'mongodb://localhost/hungry-ghost',
   }
 };
